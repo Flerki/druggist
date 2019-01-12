ce841b07e6694c5972a7177102352af5d0f92ce7
Dafalo <dl23sorry@mail.ru>
2019-01-12 16:36:29 +0300

    add Category controller

---
+++ src/main/java/app/controllers/CategoryController.java
@@ -0,0 +1,47 @@
+package app.controllers;
+
+import app.domain.model.Category;
+import app.domain.model.User;
+import app.services.AuthService;
+import app.services.CategoryService;
+import app.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("user/{userId}/category")
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    AuthService authService;
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public void create(@RequestBody Category category, @PathVariable String userId){
+        categoryService.createCategory(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id, @PathVariable int userId, @RequestHeader String auhorization){
+        categoryService.deleteCategory(id);
+    }
+    @PutMapping("/update/{id}")
+    public void update(@RequestBody Category category, @PathVariable int userId, @RequestHeader String authorization) {
+        User user = userService.findById(userId);
+        authService.checkAuthentication(user, authorization);
+        categoryService.updateCategory(category);
+    }
+    @GetMapping("/allmeds")
+    public List<Category> getAll(@PathVariable int userId, @RequestHeader String authorization) {
+        User user = userService.findById(userId);
+        authService.checkAuthentication(user, authorization);
+        return categoryService.getAll();
+    }
+
+}
