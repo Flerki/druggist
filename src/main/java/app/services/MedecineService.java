c97d3281b4dedeb1064c3028b27a09c9300e8603
Andrei <mairovichaa@yandex.ru>
2019-01-20 14:43:08 +0300

    Remove unnecessary services' interfaces

--- src/main/java/app/services/MedecineService.java
+++ src/main/java/app/services/MedecineService.java
@@ -1,17 +1,52 @@
 package app.services;
 
 import app.domain.model.Medecine;
+import app.domain.MedicineRepository;
 import app.domain.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface MedecineService {
-    void create(Medecine medecine);
-    void delete(int id);
-    void update(Medecine medecine);
-    List<Medecine> getAll();
-    List<Medecine> getExpired();
-    List<Medecine> getActive();
-    Medecine getById(int id);
-    List<Medecine> findByOwner(User owner);
+@Service
+public class MedecineService {
+    @Autowired
+    MedicineRepository medicineRepository;
+
+    public void create(Medecine medecine) {
+        if (medicineRepository.findById(medecine.getId()).isPresent()) {
+
+        }
+        medicineRepository.save(medecine);
+    }
+
+    public void delete(int id) {
+        medicineRepository.deleteById(id);
+    }
+
+    public void update(Medecine medecine) {
+        if (medicineRepository.findById(medecine.getId()).isPresent()) {
+            medicineRepository.save(medecine);
+        }
+    }
+
+    public List<Medecine> getAll() {
+        return medicineRepository.findAll();
+    }
+
+    public List<Medecine> getExpired() {
+        return medicineRepository.findExpired();
+    }
+
+    public List<Medecine> getActive() {
+        return medicineRepository.findActive();
+    }
+
+    public Medecine getById(int id) {
+        return medicineRepository.findById(id).get();
+    }
+
+    public List<Medecine> findByOwner(User owner) {
+        return medicineRepository.findByOwner(owner);
+    }
 }
