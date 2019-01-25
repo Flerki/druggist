7d54ec326a5e96229977d73feb5f05874dff88ad
amairovi <andrei.mairovich@electrolux.com>
2019-01-25 17:30:55 +0300

    Add categories to test data

--- src/main/java/app/config/TestDataConfiguration.java
+++ src/main/java/app/config/TestDataConfiguration.java
@@ -1,10 +1,11 @@
 package app.config;
 
+import app.domain.CategoryRepository;
 import app.domain.UserRepository;
+import app.domain.model.Category;
 import app.domain.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 
@@ -12,10 +13,13 @@ import javax.annotation.PostConstruct;
 public class TestDataConfiguration {
 
     private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
 
     @Autowired
-    public TestDataConfiguration(UserRepository userRepository) {
+    public TestDataConfiguration(UserRepository userRepository,
+                                 CategoryRepository categoryRepository) {
         this.userRepository = userRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @PostConstruct
@@ -24,6 +28,16 @@ public class TestDataConfiguration {
         user.setEmail("email");
         user.setLogin("login");
         user.setPassword("pass1");
+
+
         userRepository.save(user);
+
+
+        for (int i = 0; i < 4; i++) {
+            Category category = new Category();
+            category.setName("category_" + i);
+            category.setOwner(user);
+            categoryRepository.save(category);
+        }
     }
 }
